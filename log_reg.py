5c343dbe0b7057fd3dfd3e14087f2c98a8c2065c
Mia Ellis-Einhorn <melliseinh@mias-air-2.haverford.edu>
2024-04-28 21:56:56 -0400

    logreg

--- log_reg.py
+++ log_reg.py
@@ -1,15 +1,32 @@
 #import pandas
 import pandas as pd
 import util
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
 
-def process_data(csv_name):
-    #col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
-    # load dataset
+# def train_step(model):
+#     #col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
+#     # load dataset
+    
 
-    label_col = util.opt
-    food_data = pd.read_csv(csv_name, header=0)
+    # label_col = util.opt
 
-    feature_cols = []
-    label_col = food_data[]
+def run_model(csv_name):
+    X_train, X_test, y_train, y_test = util.process_txt(csv_name)
+    logreg = LogisticRegression(random_state=16)
+    logreg.fit(X_train, y_train)
 
-    X = pima[feature_cols]
+    y_pred = logreg.predict(X_test)
+
+    cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
+    print(cnf_matrix)
+
+    # train_step(logreg, X_train, X_test, y_train,
+
+
+def main():
+    # test two layer function
+    run_model("NeighborhoodFoodRetail.csv")
+
+if __name__ == "__main__":
+    main()
