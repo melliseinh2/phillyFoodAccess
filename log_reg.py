52bb66b425c5a21d8132236cd8a84908a9c568d7
melliseinh <melliseinh>
2024-04-30 14:34:41 -0400

    fc nn not working

--- log_reg.py
+++ log_reg.py
@@ -15,15 +15,17 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
     # label_col = util.opt
 
-def run_model(csv_name, label_name):
-    X_train, X_test, y_train, y_test = util.process_txt(csv_name, label_name)
+def run_model(X_train, X_test, y_train, y_test, label_name):
+    
     logreg = LogisticRegression(random_state=16, max_iter=500)
     logreg.fit(X_train, y_train)
 
     y_pred = logreg.predict(X_test)
 
+    prob_pos = logreg.predict_proba(X_test)
+
     accuracyLog = accuracy_score(y_test, y_pred)
-    print("Logistic Regression accuracy: ", accuracyLog*100, "%")
+    print(f'Logistic Regression accuracy {label_name}:', accuracyLog*100, "%")
 
     cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
     print(cnf_matrix)
@@ -32,15 +34,32 @@ def run_model(csv_name, label_name):
     plt.title(f'Logistic Regression predicting {label_name}')
     plt.savefig(f'cm_logreg_{label_name}.png')
 
+    return prob_pos
+
 
     # train_step(logreg, X_train, X_test, y_train,
 
+def visual(prob_pos_A, prob_pos_B, labels):
+    plt.clf()
+    plt.scatter(prob_pos_A, prob_pos_B)
+    plt.xlabel(labels[0])
+    plt.ylabel(labels[1])
+    plt.title("Probability of Positive by Label Predicted")
+
+    plt.savefig("probpos.png")
 
 def main():
     # test two layer function
-    opts = util.parse_args()
-    label_col = opts.label_col
-    run_model("NeighborhoodFoodRetail.csv", label_col)
+    # opts = util.parse_args()
+    label_col = ["SUPERMARKET_ACCESS", "HIGH_POVERTY"] #opts.label_col
+    prob_pos = [] 
+    df = util.process_txt("NeighborhoodFoodRetail.csv")
+    for i in label_col:
+        X_train, X_test, y_train, y_test = util.split_data(df, i)
+        prob_pos.append(run_model(X_train, X_test, y_train, y_test, i))
 
+    visual(prob_pos[0], prob_pos[1], label_col)
+
+    
 if __name__ == "__main__":
     main()
--- util.py
+++ util.py
@@ -9,14 +9,14 @@ import optparse
 import sys
 import numpy as np
 
-def process_txt(csv_name, label_col):
+def process_txt(csv_name):
     """ 
     Helper function to process a text file specified by txt_name into encoded text
     """
     
     df = pd.read_csv(csv_name, na_filter= True, header=0)
     print(df.columns)
-    df = df.drop(columns=["HPSS_ACCESS", "GEOID10", "OBJECTID", "Shape__Area", "Shape__Length", "PCT_POVERTY"]) # drop due to the label being "moderate to high" too complicated to binarize. 
+    df = df.drop(columns=["HPSS_ACCESS", "GEOID10", "OBJECTID", "Shape__Area", "Shape__Length", "PCT_POVERTY", "HPSS_ACCESS"]) # drop due to the label being "moderate to high" too complicated to binarize. 
     print(df.columns)
     for col in df.columns:
         # print(col)
@@ -24,54 +24,43 @@ def process_txt(csv_name, label_col):
         # print(df[col][1] == 'Yes' or df[col][1] == 'No' )
         if df[col][1] == 'Yes' or df[col][1] == 'No':
             df[col] = (df[col] == 'Yes').astype(int)
+    # print(df.shape)
     df = df.dropna() # drop na values. 
-    #print(df.shape)
-    # print(df.dropna().shape)
-    #print(np.any(np.isnan(df)))
-    #print(np.all(np.isfinite(df)))
+    # shuffle the datafram
+
+    print(df)
+    df = df.sample(frac= 1)
+    print(df)
+
+    return df
+
+def split_data(df, label_col):
+
     y = df[label_col]
     X = df.loc[:, df.columns != label_col]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-
-    # train_dset = tf.data.Dataset.from_tensor_slices((X_train, y_train)).shuffle(buffer_size=10000).batch(64)
-    # val_dset = tf.data.Dataset.from_tensor_slices((X_val, y_val)).batch(64)
-    # test_dset = tf.data.Dataset.from_tensor_slices((X_test, y_test)).batch(64)
-    
-
-    # splitting the dataset into train, val, and test subsets 
-    # train_index = int(df.shape[0]*0.8)
-    # val_index = int(df.shape[0]*0.9)
-
-    # train = encode_txt[:train_index]
-    # val = encode_txt[train_index:val_index]
-    # test = encode_txt[val_index:]
-
-    #train = train.shuffle()
-    # print(X_train.isnull())
-    print(X_train)
-    print(y_train)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, shuffle=False )
 
     return X_train, X_test, y_train, y_test
 
-def parse_args():
-    """Parse command line arguments (train and test arff files)."""
-    parser = optparse.OptionParser(description='run emsemble method')
+# def parse_args():
+#     """Parse command line arguments (train and test arff files)."""
+#     parser = optparse.OptionParser(description='run emsemble method')
 
-    parser.add_option('-r', '--train_filename', type='string', help='path to' +\
-        ' train arff file')
-    parser.add_option('-e', '--test_filename', type='string', help='path to' +\
-        ' test arff file')
-    parser.add_option('-l', '--label_col', type='string', help='CSV column to treat as label')
+#     parser.add_option('-r', '--train_filename', type='string', help='path to' +\
+#         ' train arff file')
+#     parser.add_option('-e', '--test_filename', type='string', help='path to' +\
+#         ' test arff file')
+#     parser.add_option('-l', '--label_col', type='string', help='CSV column to treat as label')
 
 
-    (opts, args) = parser.parse_args()
+#     (opts, args) = parser.parse_args()
 
-    mandatories = ['label_col']
-    for m in mandatories:
-        if not opts.__dict__[m]:
-            print('mandatory option ' + m + ' is missing\n')
-            parser.print_help()
-            sys.exit()
+#     # mandatories = ['label_col']
+#     # for m in mandatories:
+#     #     if not opts.__dict__[m]:
+#     #         print('mandatory option ' + m + ' is missing\n')
+#     #         parser.print_help()
+#     #         sys.exit()
 
-    return opts
+#     return opts
 
